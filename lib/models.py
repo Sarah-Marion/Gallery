96014e7e4114b396bd53b02ba14a99cb2c2fc81e
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-07 22:55:04 +0300

    created a class model for lib

---
+++ lib/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+class LibGroup(models.Model):
+
+    title = models.CharField(max_length=30, default='')
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(LibGroup, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('lib.views.lib_detail', args=[str(self.id)])
