120103024bc0933bba37cae19e58889d4183ef69
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-08 08:11:01 +0300

    defined the lib details and retuned the values in specified template

--- lib/views.py
+++ lib/views.py
@@ -6,3 +6,18 @@ from .models import LibGroup
 def libs(request):
     lib_groups = LibGroup.objects.all().order_by('title')
     return render(request, 'lib/libs.html', {'libGroups': lib_groups})
+
+
+def lib_detail(request, lib_title_slug):
+    context_dict = {}
+
+    try:
+        lib_groups = LibGroup.objects.get(slug=lib_title_slug)
+        context_dict['libGroups'] = lib_groups
+        techniques = technique.objects.filter(group=lib_groups).order_by('published_date')
+        context_dict['techniques'] = techniques
+
+    except LibGroup.DoesNotExist:
+        pass
+
+    return render(request, 'lib/lib_detail.html', context_dict)
