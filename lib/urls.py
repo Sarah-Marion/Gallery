9472534bea538ca2ba8cd3e19f44e8896fa7f3d1
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-08 13:05:31 +0300

    set the path for the about and craft for redirection

--- lib/urls.py
+++ lib/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     url(r'^$', views.libs, name='home'),
-    
+    url(r'^lib/(?P<lib_title_slug>[\w\-]+)/', views.lib_detail, name='lib_detail'),
+    url(r'^craft/(?P<craft_id>[0-9]+)/$', views.craft_detail, name='craft'),
+    url(r'^about/$', views.about_me, name='aboutMe'),
 ]
