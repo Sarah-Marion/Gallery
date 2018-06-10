5b2a4087d0a7d2de307fb32847d002c99a35594a
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-10 22:06:09 +0300

    registered Image, Category and Location

--- lib/admin.py
+++ lib/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LibGroup, Technique, Location , Category
+from .models import LibGroup, Technique, Location , Category, Image
 
 
 # Register your models here.
@@ -8,3 +8,4 @@ admin.site.register(LibGroup)
 admin.site.register(Technique)
 admin.site.register(Location)
 admin.site.register(Category)
+admin.site.register(Image)
