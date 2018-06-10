69d37ef9b035ee270d1c4008077dee55df8fb24f
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-10 22:06:46 +0300

    added file paths and fixed a file path routing

--- lib/urls.py
+++ lib/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from .import views
 
 urlpatterns = [
     url(r'^$', views.libs, name='home'),
+    url(r'^$', views.all_images, name='allPhoto'),
     url(r'^lib/(?P<lib_title_slug>[\w\-]+)/', views.lib_detail, name='lib_detail'),
     url(r'^craft/(?P<craft_id>[0-9]+)/$', views.craft_detail, name='craft'),
     url(r'^about/$', views.about_me, name='aboutMe'),
-    url(r'^location/',views.image_by_location,name='locationPhoto'),
+    url(r'^location/(?P<location>\w+)/',views.image_by_location,name='locationPhoto'),
+    url(r'^category/',views.image_by_category,name='categoryPhoto'),
+    url(r'^image/(?P<image_id>\d+)/$',views.display_details,name='photoDetails'),
+    url(r'^search/',views.search_image,name='searchPhoto'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
