7ab746f6dae312c90e80f3214a760898695cb1cb
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-10 22:08:08 +0300

    fixed errors

--- lib/views.py
+++ lib/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect, render_to_resp
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .models import LibGroup, Technique, Location
+from .models import LibGroup, Technique, Location, Category, Image
 
 
 # Create your views here.
 def libs(request):
     lib_groups = LibGroup.objects.all().order_by('title')
-    return render(request, 'lib/libs.html', {'libGroups': lib_groups})
+    categories = Category.objects.all()
+    location = Location.objects.all()
+    images = Image.objects.all()
+
+    return render(request, 'lib/libs.html', {'libGroups': lib_groups, 'categories':categories, 'location':location, 'images':images})
 
 
 def lib_detail(request, lib_title_slug):
@@ -49,11 +53,54 @@ def craft_detail(request, craft_id):
 def about_me(request):
     return render(request, 'lib/about_me.html')
 
-def image_by_location(request):
-    if request.method == "GET" and 'location_name' in request.GET and request.is_ajax():
-        location = request.GET['location_name']
-        images_location = Image.get_image_by_location(location)
 
-        return render_to_response('lib/location.html', {'images_location':images_location})
+def all_images(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    location = Location.objects.all()
+
+    return render(request,'lib/libs.html',{'images':images,'categories':categories,'location':location})
+
+
+def image_by_location(request, location):
+    # if request.method == "GET" and 'location_name' in request.GET and request.is_ajax():
+    # location = request.GET['location_name']
+    images_location = Image.get_image_by_location(location)
+
+    return render_to_response('lib/location.html', {'images_location':images_location})
+
+    # return redirect(all_images)
+
+
+def image_by_category(request):
+    if request.method == "GET" and 'category_name' in request.GET and request.is_ajax():
+        category = request.GET['category_name']
+        images_category = Image.get_image_by_category(category)
+
+        return render_to_response('lib/category.html',{'images_category':images_category})
 
     return redirect(all_images)
+
+
+
+def display_details(request,image_id):
+    categories = Category.objects.all()
+    location = Location.objects.all()
+    this_image = Image.get_image_by_id(image_id)
+
+    return render(request,'lib/image.html',{'this_image':this_image,'categories':categories,'location':location})
+
+
+def search_image(request):
+    categories = Category.objects.all()
+    location = Location.objects.all()
+    
+    if 'image-search' in request.GET and request.GET['image-search']:
+        search_category = request.GET.get('image-search')
+        images_result = Image.get_image_by_category(search_category)
+        message = f'{search_category}'
+
+        return render(request,'lib/search.html',{'images_result':images_result,'message':message,'categories':categories,'location':location})
+
+    else:
+        return render(request,'lib/search.html')
