2170d7c7cb45c9ea49debf74cb24b9ab683884c0
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-10 22:07:13 +0300

    fixed typos

--- lib/models.py
+++ lib/models.py
@@ -74,22 +74,47 @@ class Category(models.Model):
     def update_category(cls, id, new_category):
         cls.onjects.filter(id=id).update(category_name=new_category)
 
-# class Image(models.Model):
-#     image_name = models.CharField(max_length=100)
-#     image_link = models.CharField()
-#     image_description = models.TextField()
-#     image_location = models.ForeignKey('Location', null=True)
-#     image_category = models.ForeignKey('Category', null=True)
-#     image_time_created = models.DateTimeField(auto_now_add=True)
+class Image(models.Model):
+    image_name = models.CharField(max_length=100)
+    image_link = models.ImageField(upload_to='gallery/')
+    image_description = models.TextField()
+    image_location = models.ForeignKey(Location, null=True)
+    image_category = models.ForeignKey(Category, null=True)
+    image_time_created = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.image_description
+    def __str__(self):
+        return self.image_description
 
-#     class Meta:
-#         ordering=['image_name']
+    class Meta:
+        ordering=['image_name']
 
-#     def save_image(self):
-#         self.save()
+    def save_image(self):
+        self.save()
 
-#     def delete_image(self):
-#         self.delete()
+    def delete_image(self):
+        self.delete()
+
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image_link=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def get_image_by_name(cls, name):
+        images = cls.objects.filter(image_name__icontains=name).all()
+        return images
+
+    @classmethod
+    def get_image_by_location(cls, location):
+        images = Image.objects.filter(image_location__location_name=location)
+        return images
+
+    @classmethod
+    def get_image_by_category(cls, category):
+        images = cls.objects.filter(image_category__category_name__icontains=category).all()
+        return images
