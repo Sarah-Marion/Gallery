da7827c24bbaeccbfc5d40815703170f562d6093
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-12 08:23:54 +0300

    deploying to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lib',
     # 'lib.apps.LibConfig',
-    'bootstrap3',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'gallery.urls'
